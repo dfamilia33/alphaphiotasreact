c4302f9f4abfea9d9d0d3cc13ff9392df3fc2e17
dfamilia33 <dfamilia33@gmail.com>
2020-05-15 15:01:39 -0400

    useStaticQuery bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,26 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/src/img/`,
+      },
+    },
+    `gatsby-transformer-sharp`, 
+    `gatsby-plugin-sharp`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,37 @@
 import React from "react"
 import ReactDOM from 'react-dom';
 import Toolbar from "../components/toolbar"
+import Img from "gatsby-image"
+import { useStaticQuery, graphql} from "gatsby"
 
 
 export default function Index() {
-    
+
+    const data = useStaticQuery(graphql`
+      query MyQuery {
+        file(relativePath: {eq: "mainFIA.jpg"}) {
+          childImageSharp {
+            fluid {
+              aspectRatio
+              base64
+              sizes
+              src
+              srcSet
+            }
+          }
+        }
+      }
+    `);
+
+  
     return (
-        <Toolbar/>      
+        <div>
+            <Toolbar/> 
+            <Img fluid = {data.childImageSharp.fluid}
+            alt = "Chapter Photo"
+            />
+        </div>
+             
     )
 
 }

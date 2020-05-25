66b332f9cf48d43b9647927cb721e8ae6fc5f626
dfamilia33 <dfamilia33@gmail.com>
2020-05-24 22:54:56 -0400

    Card as a class

--- src/components/card.js
+++ src/components/card.js
@@ -1,66 +1,79 @@
 import React from "react"
 
+export default class BootstrapCard extends React.Component {
 
-export default function BootstrapCard(props) {
-
-    let src = "https://via.placeholder.com/400x400.png?text=Placeholder";
-
-    if (props.bro.src !== undefined){
-        src = props.bro.src;
+    constructor(props) {
+      super(props);
+      this.state = {
+        selected: false,
+        hovered: false,
+      };
     }
 
-    return(
-        <div className="card bg-dark text-white" style={{width: "18rem", margin: "20px",boxShadow: "0px 1px 20px 5px  #b30000"}}>
-          <div className="brotherimgbackdrop card-img-top" style={{backgroundImage:`url("${src}")`}}>
-            <div className="brotheroverlay" style={{overflow:"hidden"}}>
-              <div style={{color:"white", padding:20}}>
-                <div style={{float:"left",width:110}}>Brother Name:</div><div style={{float:"left", width:130}}>{props.bro.bname}</div>
-                <div style={{float:"left",width:40}}>Line:</div><div style={{float:"left",width:200}}>{props.bro.line}</div> 
-                <div style={{float:"left",width:50}}>Major:</div><div style={{float:"left",width:190}}>
-                  <ul style={{listStyleType: "none", padding:0,margin:0}}>
-                    {props.bro.major.map((major,index) => ( 
-                        <li>{major}</li>
-                    ))}
-                  </ul>
+
+
+    render(){
+
+      let src = "https://via.placeholder.com/400x400.png?text=Placeholder";
+
+      if (this.props.bro.src !== undefined){
+          src = this.props.bro.src;
+      }
+
+      return(
+          <div key={this.props.bro.bname} className="card bg-dark text-white" style={{width: "18rem", margin: "20px"}}>
+            <div className="brotherimgbackdrop card-img-top" style={{backgroundImage:`url("${src}")`}}>
+              <div className="brotheroverlay" style={{overflow:"hidden"}}>
+                <div style={{color:"white", padding:20}}>
+                  <div style={{float:"left",width:110}}>Brother Name:</div><div style={{float:"left", width:130}}>{this.props.bro.bname}</div>
+                  <div style={{float:"left",width:40}}>Line:</div><div style={{float:"left",width:200}}>{this.props.bro.line}</div> 
+                  <div style={{float:"left",width:50}}>Major:</div><div style={{float:"left",width:190}}>
+                    <ul style={{listStyleType: "none", padding:0,margin:0}}>
+                      {this.props.bro.major.map((major,index) => ( 
+                          <li>{major}</li>
+                      ))}
+                    </ul>
+                  </div>
+                  {this.props.bro.minor !== undefined &&
+                      (
+                      <div>
+                          <div style={{float:"left",width:50}}>Minor:</div>
+                          <div style={{float:"left",width:190}}>
+                              <ul style={{listStyleType: "none", padding:0,margin:0}}>
+                              {this.props.bro.minor.map((minor,index) => ( 
+                                  <li>{minor}</li>
+                              ))}
+                              </ul>
+                          </div>
+                      </div>
+                      )
+
+                  }
+                  <div style={{float:"left",width:90}}>Graduation:</div><div style={{float:"left",width:150}}>{this.props.bro.graduation}</div>
+                  <div style={{float:"left",width:90}}>Hometown:</div><div style={{float:"left",width:150}}>{this.props.bro.hometown}</div>
+                  <div style={{float:"left",width:90}}>Nationality:</div><div style={{float:"left",width:150}}>   
+                    <ul style={{listStyleType: "none", padding:0,margin:0}}>
+                      {this.props.bro.nationality.map((nationality,index) => ( 
+                          <li>{nationality} <img src={this.props.bro.flagsrc[index]} alt={`${nationality} Flag`} style ={{width: 20,height:15}}/> </li> 
+                      ))}
+                    </ul>
+                  </div> 
+                  {this.props.bro.position !== undefined &&
+                      (
+                          <div>
+                              <div style={{float:"left",width:70}}>Position:</div>
+                              <div style={{float:"left",width:170}}>{this.props.bro.position}</div>
+                          </div>
+                      )
+
+                  }
                 </div>
-                {props.bro.minor !== undefined &&
-                    (
-                    <div>
-                        <div style={{float:"left",width:50}}>Minor:</div>
-                        <div style={{float:"left",width:190}}>
-                            <ul style={{listStyleType: "none", padding:0,margin:0}}>
-                            {props.bro.minor.map((minor,index) => ( 
-                                <li>{minor}</li>
-                            ))}
-                            </ul>
-                        </div>
-                    </div>
-                    )
-
-                }
-                <div style={{float:"left",width:90}}>Graduation:</div><div style={{float:"left",width:150}}>{props.bro.graduation}</div>
-                <div style={{float:"left",width:90}}>Hometown:</div><div style={{float:"left",width:150}}>{props.bro.hometown}</div>
-                <div style={{float:"left",width:90}}>Nationality:</div><div style={{float:"left",width:150}}>   
-                  <ul style={{listStyleType: "none", padding:0,margin:0}}>
-                    {props.bro.nationality.map((nationality,index) => ( 
-                        <li>{nationality} <img src={props.bro.flagsrc[index]} alt={`${nationality} Flag`} style ={{width: 20,height:15}}/> </li> 
-                    ))}
-                  </ul>
-                </div> 
-                {props.bro.position !== undefined &&
-                    (
-                        <div>
-                            <div style={{float:"left",width:70}}>Position:</div>
-                            <div style={{float:"left",width:170}}>{props.bro.position}</div>
-                        </div>
-                    )
-
-                }
               </div>
             </div>
+            <div className="card-footer">{this.props.bro.name}</div>    
           </div>
-          <div className="card-footer">{props.bro.name}</div>    
-        </div>
-    )
+      )
+
+  }
 
 }

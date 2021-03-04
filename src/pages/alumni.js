f442cbcd69f5b73090bbbb2fab1133f574ebd0be
dfamilia33 <dfamilia33@gmail.com>
2021-03-04 17:55:25 -0500

    Update Alumni Page

--- src/pages/alumni.js
+++ src/pages/alumni.js
@@ -3,6 +3,7 @@ import Toolbar from "../components/toolbar"
 import Application from "../components/app"
 import Footer from "../components/footer"
 import paypal from "../../static/src/img/paypal.png"
+import Button from 'react-bootstrap/Button'
 
 
 export default function Alumni() {
@@ -18,15 +19,27 @@ export default function Alumni() {
               <h2 className="overheader">Alumni</h2>       
           </div>
         </div>
-        <div style={{height:"85vh"}}>
-            <img style={{width:"300px"}} src={paypal} alt= "Paypal logo"></img>
-            <form action="https://www.paypal.com/donate" method="post" target="_top">
-                <input type="hidden" name="cmd" value="_donations" />
-                <input type="hidden" name="business" value="dfamilia33@gmail.com" />
-                <input type="hidden" name="currency_code" value="USD" />
-                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-            </form>
+        <div style={{color: `white`,textAlign:"center", height:"50vh"}}>
+          <h1 style={{ margin:"50px",textShadow: "0px 0px 5px white" }}>Alumni Weekend 2021</h1>
+          <p>Details To Be Announced...</p>
+        </div>
+
+
+
+
+        <div style={{height:"50vh"}}>
+          <h4 style={{color:"white",textAlign:"center"}}> Make a Contribution</h4>
+          <div className="flex-paypal" style={{ width:"50%", marginLeft:"25%",marginRight:"25%"}}>
+              <img style={{width:"300px"}} src={paypal} alt= "Paypal logo"></img>
+              <form action="https://www.paypal.com/donate" method="post" target="_top">
+                  <input type="hidden" name="cmd" value="_donations" />
+                  <input type="hidden" name="business" value="treasurer@phiota.net" />
+                  <input type="hidden" name="currency_code" value="USD" />
+                  
+                  <Button type="submit" color="primary">Donate</Button>
+                  
+              </form>
+          </div>
         </div>
         <Footer/>
       </div>
